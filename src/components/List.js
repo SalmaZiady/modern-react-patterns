28184feb929e601a49daece6795ca1a19fd94956
SalmaZiady <salma.elziady@yahoo.com>
2026-02-06 00:54:48 +0200

    Add useEffect hook implementation

--- src/components/List.js
+++ src/components/List.js
@@ -1,27 +1,32 @@
-import { useState } from "react"
-import Child from './Child'
+import { useEffect, useState } from "react";
+import Child from "./Child";
 
-const List = ({name = "John Doe", designation = "Software Engineer"}) => {
+const List = ({ name = "John Doe", designation = "Software Engineer" }) => {
+  const [details, setDetails] = useState({
+    name: "David",
+    designation: "Software Engineer",
+  });
 
-     const [details, setDetails] = useState({
-        name:"David",
-        designation: "Software Engineer"
-    })
+  const updateState = () => {
+    setDetails({
+      ...details,
+      name: "Salma Ziady",
+    });
+  };
 
-    const updateState = () => {
-       setDetails({
-          ... details,
-          name:"Salma Ziady"
-       })
-    }
+  useEffect(() => {
+    setTimeout(() => {
+      console.log("I am coming from useEffect");
+    }, 2000);
+  }, []);
 
   return (
     <div className="list">
       <h2>Use State Hook in React </h2>
-      <Child details = {details} />
+      <Child details={details} />
       <button onClick={updateState}> Update State </button>
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
