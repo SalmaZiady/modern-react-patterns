2f902445dd59df34f5e7ae2cadc415b7deed3a9b
SalmaZiady <salma.elziady@yahoo.com>
2026-02-05 23:18:23 +0200

    Update List component with default parameters

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,8 @@
-const List = ({list}) => {
-
+const List = ({name = "John Doe", designation = "Software Engineer"}) => {
   return (
     <div className="list">
-        <h2>List component</h2>
-        <p>{list.toString()}</p>
+      <h2>List component</h2>
+      <p>my name is {name} and I am working as {designation}</p>
     </div>
   )
 }
