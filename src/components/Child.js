1b89b3354659c51b52f92af2ce2d5ab546cc8385
SalmaZiady <salma.elziady@yahoo.com>
2026-02-06 00:17:13 +0200

    Add useState hook implementation

---
+++ src/components/Child.js
@@ -0,0 +1,8 @@
+
+const Child = ({details}) => {
+  return (
+   <p> my name is {details.name} and I am working as {details.designation} </p>
+  )
+}
+
+export default Child
--- src/components/List.js
+++ src/components/List.js
@@ -1,8 +1,25 @@
+import { useState } from "react"
+import Child from './Child'
+
 const List = ({name = "John Doe", designation = "Software Engineer"}) => {
+
+     const [details, setDetails] = useState({
+        name:"David",
+        designation: "Software Engineer"
+    })
+
+    const updateState = () => {
+       setDetails({
+          ... details,
+          name:"Salma Ziady"
+       })
+    }
+
   return (
     <div className="list">
-      <h2>List component</h2>
-      <p>my name is {name} and I am working as {designation}</p>
+      <h2>Use State Hook in React </h2>
+      <Child details = {details} />
+      <button onClick={updateState}> Update State </button>
     </div>
   )
 }
