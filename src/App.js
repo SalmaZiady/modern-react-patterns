01f5be3a1d0fc609d847e434c12ea72d279c7d85
SalmaZiady <salma.elziady@yahoo.com>
2026-02-05 22:54:16 +0200

    Add component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import List from './components/List';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <><Header />
+    <List />
+    <Footer /></>
+  );
+}
+
+export default App;
