d839bd75cbad6369403d3dad5169f9bcfb4fc5c8
SalmaZiady <salma.elziady@yahoo.com>
2026-02-06 16:43:22 +0200

    Add list rendering with keys

--- src/components/List.js
+++ src/components/List.js
@@ -1,18 +1,29 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Child from "./Child";
 
-const List = ({ name = "John Doe", designation = "Software Engineer" }) => {
+/* 
+ const List = ({ name = "John Doe", designation = "Software Engineer" }) => {
   const [details, setDetails] = useState({
     name: "David",
     designation: "Software Engineer",
   });
+  */
 
+const List = () => {
+  const [list, setList] = useState([
+    { name: "John", designation: "Backend Engineer" },
+    { name: "Jane", designation: "QA Engineer" },
+    { name: "David", designation: "Frontend Enigneer" },
+  ]);
+
+  /*
   const updateState = () => {
     setDetails({
       ...details,
       name: "Salma Ziady",
     });
   };
+  
 
   useEffect(() => {
     setTimeout(() => {
@@ -20,11 +31,30 @@ const List = ({ name = "John Doe", designation = "Software Engineer" }) => {
     }, 2000);
   }, []);
 
+  const inputRef = useRef(null);
+
+  const getValue = () => {
+    inputRef.current.focus();
+  };
+
+  */
+
+
+  // map through the array
+  // return a jsx element in map method
+  // assign a key props inside your jsx element
+
   return (
     <div className="list">
-      <h2>Use State Hook in React </h2>
-      <Child details={details} />
-      <button onClick={updateState}> Update State </button>
+      <h2> List in React </h2>
+      <div className="cards">
+        {list.map((list, index ) => (
+          <div className="card" key={index}>
+            <h1>{list.name}</h1>
+            <p>  {list.designation} </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
